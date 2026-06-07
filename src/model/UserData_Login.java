a94a008acea06e992658bc20169c95a5e8fc68e6
siddharth <siddharthkathayat115@gmail.com>
2026-06-07 11:41:21 +0545

    admin and agent dashboard

---
+++ src/model/UserData_Login.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Dell
+ */
+public class UserData_Login {
+    private int id;
+
+    public int getId(){
+
+        return id;
+
+    }
+
+    public void setId(int id){
+
+        this.id=id;
+
+    }
+
+    private String username;
+
+    public String getUsername(){
+
+        return username;
+
+    }
+
+    public void setUsername(String username){
+
+        this.username=username;
+
+    }
+
+     
+    private String password;
+
+    public String getPassword(){
+
+        return password;
+
+    }
+
+    public void setPassword(String password){
+
+        this.password=password;
+
+    }
+
+}
+ 
+
